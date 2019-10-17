4c54bdb8dea027f900172cdbd9e2e2a886a4ae93
kreuzerk <kevin.kreuzer90@icloud.com>
2019-10-17 20:58:02 +0200

    chore(prettier): add and run prettier

--- projects/pretty-html-log-showcase/e2e/src/app.e2e-spec.ts
+++ projects/pretty-html-log-showcase/e2e/src/app.e2e-spec.ts
@@ -10,14 +10,21 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('pretty-html-log-showcase app is running!');
+    expect(page.getTitleText()).toEqual(
+      'pretty-html-log-showcase app is running!'
+    );
   });
 
   afterEach(async () => {
     // Assert that there are no errors emitted from the browser
-    const logs = await browser.manage().logs().get(logging.Type.BROWSER);
-    expect(logs).not.toContain(jasmine.objectContaining({
-      level: logging.Level.SEVERE,
-    } as logging.Entry));
+    const logs = await browser
+      .manage()
+      .logs()
+      .get(logging.Type.BROWSER);
+    expect(logs).not.toContain(
+      jasmine.objectContaining({
+        level: logging.Level.SEVERE
+      } as logging.Entry)
+    );
   });
 });
--- projects/pretty-html-log/src/lib/pretty-html-log.ts
+++ projects/pretty-html-log/src/lib/pretty-html-log.ts
@@ -1,12 +1,18 @@
-import {highlight} from 'pretty-html-log';
-import {Theme} from 'pretty-html-log';
-import {ComponentFixture} from '@angular/core/testing';
+import { highlight } from 'pretty-html-log';
+import { Theme } from 'pretty-html-log';
+import { ComponentFixture } from '@angular/core/testing';
 
-export const prettyPrintFixture = <T>(fixture: ComponentFixture<T>, theme?: Theme): void => {
+export const prettyPrintFixture = <T>(
+  fixture: ComponentFixture<T>,
+  theme?: Theme
+): void => {
   console.log(prettyFixture(fixture, theme));
 };
 
-export const prettyFixture = <T>(fixture: ComponentFixture<T>, theme?: Theme): string => {
+export const prettyFixture = <T>(
+  fixture: ComponentFixture<T>,
+  theme?: Theme
+): string => {
   return highlight(fixture.debugElement.nativeElement.innerHTML, theme);
 };
 

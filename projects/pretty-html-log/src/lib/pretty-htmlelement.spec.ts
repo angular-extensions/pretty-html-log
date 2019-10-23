36286330d865740e86f3d58e72863d324c2ccd74
kreuzerk <kevin.kreuzer90@icloud.com>
2019-10-23 14:20:37 +0200

    test(prettyfixture): test pretty Html element

---
+++ projects/pretty-html-log/src/lib/pretty-htmlelement.spec.ts
@@ -0,0 +1,59 @@
+import * as prettyHTMLElement from './pretty-htmlelement';
+import { prettyPrintHtmlElement } from './pretty-htmlelement';
+import * as prettyHTMLLog from 'pretty-html-log';
+import { THEMES } from 'pretty-html-log';
+
+describe('pretty HTML element', () => {
+  it('should call prettyHtmlElement with the htmlElement and pass it to console.log', () => {
+    console.log = jest.fn();
+    const htmlElement = {} as HTMLElement;
+    spyOn(prettyHTMLElement, 'prettyHtmlElement');
+    prettyHTMLElement.prettyPrintHtmlElement(htmlElement, THEMES.DRACULA);
+
+    expect(console.log).toHaveBeenCalledWith(
+      prettyPrintHtmlElement(htmlElement, THEMES.DRACULA)
+    );
+  });
+
+  it(`should call prettyHtmlelement with the htmlElement for each htmlElement. It should then
+    pass the returned value to console.log
+    for each `, () => {
+    console.log = jest.fn();
+
+    const htmlElementOne = { innerText: 'HtmlElementOne' } as HTMLElement;
+    const htmlElementTwo = { innerText: 'HtmlElementTwo' } as HTMLElement;
+    const htmlElementThree = { innerText: 'HtmlElementThree' } as HTMLElement;
+
+    const htmlElements = [
+      htmlElementOne,
+      htmlElementTwo,
+      htmlElementThree
+    ] as HTMLElement[];
+    spyOn(prettyHTMLElement, 'prettyHtmlElement');
+    prettyHTMLElement.prettyPrintHtmlElements(htmlElements, THEMES.DRACULA);
+
+    expect(console.log).toHaveBeenCalledWith(
+      prettyPrintHtmlElement(htmlElementOne, THEMES.DRACULA)
+    );
+    expect(console.log).toHaveBeenCalledWith(
+      prettyPrintHtmlElement(htmlElementTwo, THEMES.DRACULA)
+    );
+    expect(console.log).toHaveBeenCalledWith(
+      prettyPrintHtmlElement(htmlElementThree, THEMES.DRACULA)
+    );
+  });
+
+  it('should call the highlight method with the innerHTML', () => {
+    const innerHTML = '<h1>Foo</h1>';
+    const htmlElement = {
+      innerHTML
+    } as HTMLElement;
+    spyOn(prettyHTMLLog, 'highlight');
+
+    prettyHTMLElement.prettyHtmlElement(htmlElement, THEMES.DRACULA);
+    expect(prettyHTMLLog.highlight).toHaveBeenCalledWith(
+      innerHTML,
+      THEMES.DRACULA
+    );
+  });
+});
